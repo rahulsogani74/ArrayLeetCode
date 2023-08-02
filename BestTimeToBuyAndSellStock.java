af7c11b92a310d433edb2c491ce2b7d9ead5ba0e
rahulsogani74 <131875485+rahulsogani74@users.noreply.github.com>
2023-08-03 00:59:23 +0530

    Add files via upload

---
+++ BestTimeToBuyAndSellStock.java
@@ -0,0 +1,18 @@
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        BestTimeToBuyAndSellStock solution = new BestTimeToBuyAndSellStock();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int maxProfit = solution.maxProfit(prices);
+        System.out.println("Maximum profit: " + maxProfit);
+    }
+}
