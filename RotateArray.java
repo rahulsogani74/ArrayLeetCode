776c3e52e946ef6c1b74718f2c39511ecb95ebbe
rahulsogani74 <131875485+rahulsogani74@users.noreply.github.com>
2023-08-18 00:27:50 +0530

    Add files via upload

---
+++ RotateArray.java
@@ -0,0 +1,28 @@
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        RotateArray solution = new RotateArray();
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        solution.rotate(nums, k);
+        System.out.println("Rotated Array:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
